cce187aa8dd4e3032cc2a2e84748a6213f0b107f
Saurav Dixit <dixit07sourav@gmail.com>
2025-03-07 12:32:26 +0530

    save

--- test_app.py
+++ test_app.py
@@ -12,3 +12,9 @@ def client():
 def test_home(client):
     resp = client.get('/sing')
     assert resp.status_code == 200
+
+def test_predict(client):
+    test_data={'Gender':"Male", 'Married':"Unmarried",'Credit_History' : "Unclear Debts",'ApplicantIncome':100000,'LoanAmount':2000000}
+    resp=client.post('/prediction', json=test_data)
+    assert resp.status_code == 200
+    assert resp.json=={'loan_approval_status': 'Rejected'}
